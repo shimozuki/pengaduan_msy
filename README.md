06f9626afcd161f0714b666022b7e46b5341feee
robbishimozuki <Onerianda@gmail.com>
2024-01-27 17:45:16 +0800

    pengaduan masyarakat

---
+++ README.md
@@ -0,0 +1,10 @@
+<img src="https://i.imgur.com/VflbSZV.png" width="400" alt="Kami peduli Logo"></a></p>
+
+## How to install
+
+1. clone the repository `git clone https://github.com/HMephisto/laravel_pengaduan_masyarakat`
+2. run `composer install`
+3. copy the .env.example to .env
+4. run  `php artisan migrate`
+5. run  `php artisan serve`  
+
---
+++ database/factories/PetugasFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Petugas>
+ */
+class PetugasFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nama_petugas' => fake()->name(),
+            'username' => fake()->name(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'telp' => rand(0,12),   
+            'level' => 'admin',   
+         ];
+    }
+}
